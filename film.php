7424cca545a6933627f24b5ac7dbba148ffe8bd4
WillyFaq <willyfaqurokhim@gmail.com>
2020-07-01 23:09:16 +0700

    fixing bug

--- film.php
+++ film.php
@@ -5,3 +5,14 @@
 	<source src="https://www.googleapis.com/drive/v3/files/<?= $list_anime['id']; ?>?key=AIzaSyDH5STT2W72g4xPocc3B-7QWh1vMRznp0s&alt=media" type='video/mp4'/>
     <source src="https://www.googleapis.com/drive/v3/files/<?= $list_anime['id']; ?>?key=AIzaSyDH5STT2W72g4xPocc3B-7QWh1vMRznp0s&alt=media" type='video/webm'/>
 </video>
+<script type="text/javascript">
+	if ('mediaSession' in navigator) {
+	  	navigator.mediaSession.metadata = new MediaMetadata({
+	    	artwork: [
+	          	{
+	            	src: '<?= $list_anime['thumbnail']; ?>', sizes: '128x128', type: 'image/png'
+	            }
+	      	]
+	    });
+	}
+</script>
